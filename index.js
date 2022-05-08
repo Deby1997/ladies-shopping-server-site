96b5559d5649ce7bcd7ade6d082b153bbb27ce12
Deby1997 <debysarker159@gmail.com>
2022-05-08 20:35:15 +0600

    last Commit

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ async function run() {
             const result = await productCollection.deleteOne(query);
             res.send(result);
         });
+        //update
+        // app.put('/inventory/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await productCollection.updateOne(query);
+        //     res.send(result);
+        // });
     }
     finally {
 
